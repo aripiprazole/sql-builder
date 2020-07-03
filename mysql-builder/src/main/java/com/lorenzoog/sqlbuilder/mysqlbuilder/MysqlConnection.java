92bdbe02f31f9327dea00e258bab8deb3988257f
Lorenzo Guimarães de Oliveira <lorenzooguimaraes@gmail.com>
2020-07-03 18:01:51 -0300

    fix(sql-api): prepare calls throws SQLException

--- mysql-builder/src/main/java/com/lorenzoog/sqlbuilder/mysqlbuilder/MysqlConnection.java
+++ mysql-builder/src/main/java/com/lorenzoog/sqlbuilder/mysqlbuilder/MysqlConnection.java
@@ -1,4 +1,37 @@
 package com.lorenzoog.sqlbuilder.mysqlbuilder;
 
-public class MysqlConnection {
+import com.lorenzoog.sqlbuilder.sqlapi.SqlBuilder;
+import com.lorenzoog.sqlbuilder.sqlapi.SqlConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MysqlConnection implements SqlConnection {
+
+    private final Connection connection;
+
+    MysqlConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public SqlBuilder table(String name) {
+        return null;
+    }
+
+    @Override
+    public PreparedStatement prepare(SqlBuilder sqlBuilder) throws SQLException {
+        return connection.prepareStatement(sqlBuilder.toString());
+    }
+
+    @Override
+    public PreparedStatement prepare(String statement) throws SQLException {
+        return connection.prepareStatement(statement);
+    }
+
+    @Override
+    public Connection getOriginalConnection() {
+        return connection;
+    }
 }
