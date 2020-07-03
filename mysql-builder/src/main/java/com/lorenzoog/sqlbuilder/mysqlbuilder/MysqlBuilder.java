c0956ebb9edcac6b0cd60debefe4fd8d9765d4e6
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-03 18:06:23 -0300

    feat(mysql-builder): add raw sql builder

---
+++ mysql-builder/src/main/java/com/lorenzoog/sqlbuilder/mysqlbuilder/MysqlBuilder.java
@@ -0,0 +1,139 @@
+package com.lorenzoog.sqlbuilder.mysqlbuilder;
+
+import com.lorenzoog.sqlbuilder.sqlapi.SqlBuilder;
+import com.lorenzoog.sqlbuilder.sqlapi.ThrowableFunction;
+import com.sun.istack.internal.Nullable;
+
+import java.sql.ResultSet;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+
+public class MysqlBuilder implements SqlBuilder {
+
+    @Nullable
+    private String rawSql = null;
+
+    @Override
+    public SqlBuilder raw(String rawSql) {
+        this.rawSql = rawSql;
+
+        return this;
+    }
+
+    @Override
+    public SqlBuilder update() {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder alter() {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder union() {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder delete() {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder drop() {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder where(String column, String operator, Object value) {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder where(String column, Object value) {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder insert(String... columns) {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder replace(String... columns) {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder select(String... columns) {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder set(String column, Object value) {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder orderBy(String column) {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder orderByDesc(String column) {
+        return null;
+    }
+
+    @Override
+    public SqlBuilder groupBy(String... column) {
+        return null;
+    }
+
+    @Override
+    public <T> CompletableFuture<T> runAsync(ExecutorService executorService, ThrowableFunction<ResultSet, T> callback) {
+        return null;
+    }
+
+    @Override
+    public <T> CompletableFuture<T> runAsync(ThrowableFunction<ResultSet, T> callback) {
+        return null;
+    }
+
+    @Override
+    public <T> T runBlocking(ThrowableFunction<ResultSet, T> callback) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<ResultSet> runAsync(ExecutorService executorService) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<ResultSet> runAsync() {
+        return null;
+    }
+
+    @Override
+    public ResultSet runBlocking() {
+        return null;
+    }
+
+    @Override
+    public boolean execute() {
+        return false;
+    }
+
+    @Override
+    public CompletableFuture<Boolean> executeAsync() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        if(rawSql != null) return rawSql;
+
+        return "";
+    }
+}
