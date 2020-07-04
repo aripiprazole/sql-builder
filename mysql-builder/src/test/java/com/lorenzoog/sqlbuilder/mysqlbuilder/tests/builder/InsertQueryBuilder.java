53049ed4cf9885a91e97c8c4a531a768940f3d5c
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-07-04 00:15:10 -0300

    test(mysql-builder): test insert query builder

---
+++ mysql-builder/src/test/java/com/lorenzoog/sqlbuilder/mysqlbuilder/tests/builder/InsertQueryBuilder.java
@@ -0,0 +1,28 @@
+package com.lorenzoog.sqlbuilder.mysqlbuilder.tests.builder;
+
+import com.lorenzoog.sqlbuilder.mysqlbuilder.MysqlConnection;
+import com.lorenzoog.sqlbuilder.sqlapi.SqlBuilder;
+import com.lorenzoog.sqlbuilder.sqlapi.SqlConnection;
+import junit.framework.TestCase;
+
+import java.sql.Connection;
+
+import static org.mockito.Mockito.*;
+
+public final class InsertQueryBuilder extends TestCase {
+
+    public void testShouldBuildSqlStringCorrectly() {
+        Connection original = mock(Connection.class);
+        SqlConnection connection = new MysqlConnection(original);
+
+        SqlBuilder builder = connection
+                .table("users")
+                .insert("username")
+                .values("Name");
+
+        System.out.println(builder);
+
+        assertEquals("INSERT INTO users(username) VALUES(?)", builder.toString());
+    }
+
+}
